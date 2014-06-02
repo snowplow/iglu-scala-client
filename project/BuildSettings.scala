aaafbdc78926a4a5f68fc0f4c465bc01591cb7f1
Alex Dean <alex@snowplowanalytics.com>
2014-06-02 13:36:17 +0100

    This is a snapshot so far

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -18,7 +18,7 @@ object BuildSettings {
   // Basic settings for our app
   lazy val basicSettings = Seq[Setting[_]](
     organization  := "com.snowplowanalytics",
-    version       := "0.1.0",
+    version       := "0.1.0-SNAPSHOT",
     description   := "Java and Scala client for Iglu",
     scalaVersion  := "2.10.4",
     scalacOptions := Seq("-deprecation", "-encoding", "utf8"),
