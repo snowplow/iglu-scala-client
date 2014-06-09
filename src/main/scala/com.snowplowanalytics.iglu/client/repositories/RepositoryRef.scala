422a6bc0645847898e7156e8c687c7d62f0c7964
Alex Dean <alex@snowplowanalytics.com>
2014-06-09 22:42:36 +0100

    Removed duplicated lookup methods

--- src/main/scala/com.snowplowanalytics.iglu/client/repositories/RepositoryRef.scala
+++ src/main/scala/com.snowplowanalytics.iglu/client/repositories/RepositoryRef.scala
@@ -40,33 +40,6 @@ abstract class RepositoryRef(val vendorPrefixes: List[String]) extends Lookup {
   def searchPriority: Int
 
   /**
-   * Abstract method. Provide a concrete
-   * implementation for how to lookup a schema
-   * in this type of repository.
-   *
-   * @param schemaKey The SchemaKey uniquely identifying
-   *        the schema in Iglu
-   * @return a Validation boxing either the Schema's
-   *         JsonNode on Success, or an error String
-   *         on Failure 
-   */
-  def lookupSchema(schemaKey: SchemaKey): ValidatedJsonNode
-
-  /**
-   * Abstract method. Provide a concrete
-   * implementation for how to lookup a schema
-   * in this type of repository in an unsafe fashion.
-   *
-   * ONLY implement in a sub-class if the resolution has
-   * a good chance of succeeding (e.g. no network I/O).
-   *
-   * @param schemaKey The SchemaKey uniquely identifying
-   *        the schema in Iglu
-   * @return the JsonNode representing this schema
-   */
-  def unsafeLookupSchema(schemaKey: SchemaKey): JsonNode
-
-  /**
    * Helper to check if this repository should take
    * priority because of a vendor prefix match. Returns
    * true if 
