9bbfaa0329649b33f3035e59642b6c21a997d099
Alex Dean <alex@snowplowanalytics.com>
2014-06-16 23:40:18 +0100

    Added a couple more tests

--- src/test/scala/com.snowplowanalytics.iglu.client/validation/SelfDescValidationSpec.scala
+++ src/test/scala/com.snowplowanalytics.iglu.client/validation/SelfDescValidationSpec.scala
@@ -13,6 +13,9 @@
 package com.snowplowanalytics.iglu.client
 package validation
 
+// This project
+import ValidatableJsonMethods._
+
 // Specs2
 import org.specs2.Specification
 import org.specs2.scalaz.ValidationMatchers
@@ -20,21 +23,28 @@ import org.specs2.scalaz.ValidationMatchers
 // TODO: add in some Failures
 class SelfDescValidationSpec extends Specification with ValidationMatchers { def is =
 
-  "This is a specification to test validation of self-describing JsonNodes"                                ^
-                                                                                                          p^
-  "validating a correct self-desc JSON should return the JSON in a Success"                                ! e1^
-  "validating a correct self-desc JSON should return only the JSON's data field in a Success if requested" ! e2^
-                                                                                                           end
+  "This is a specification to test validation of self-describing JsonNodes"                                                  ^
+                                                                                                                            p^
+  "validating a correct self-desc JSON should return the JSON in a Success"                                                  ! e1^
+  "validating a correct self-desc JSON should return only the JSON's data field in a Success if requested"                   ! e2^
+  "validating & identifying a correct self-desc JSON should return the JSON & the identified SchemaKey"                      ! e3^
+  "validating & identifying a correct self-desc JSON should return only the JSON's data field & identified key if requested" ! e4^
+                                                                                                                             end
 
-  import ValidatableJsonMethods._
+
   implicit val resolver = SpecHelpers.TestResolver
 
   val validJson = SpecHelpers.asJsonNode(
     """{"schema": "iglu:com.snowplowanalytics.iglu-test/stock-item/jsonschema/1-0-0", "data": { "id": "123-12", "name": "t-shirt", "price": 29.99 } }"""
     )
 
+  val expectedKey = SchemaKey("com.snowplowanalytics.iglu-test", "stock-item", "jsonschema", "1-0-0")
+
   def e1 = validJson.validate(false) must beSuccessful(validJson)
 
   def e2 = validJson.validate(true) must beSuccessful(validJson.get("data"))
 
+  def e3 = validJson.validateAndIdentifySchema(false) must beSuccessful((expectedKey, validJson))
+
+  def e4 = validJson.validateAndIdentifySchema(true) must beSuccessful((expectedKey, validJson.get("data")))
 }
