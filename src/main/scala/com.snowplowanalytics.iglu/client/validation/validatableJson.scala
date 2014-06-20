ae7151ea934a2430eadf2f444ef066002e26e7c8
Alex Dean <alex@snowplowanalytics.com>
2014-06-20 17:44:27 +0100

    Added in implementation for this

--- src/main/scala/com.snowplowanalytics.iglu/client/validation/validatableJson.scala
+++ src/main/scala/com.snowplowanalytics.iglu/client/validation/validatableJson.scala
@@ -154,7 +154,15 @@ object ValidatableJsonMethods {
    *         of ProcessingMessages
    */
   def verifySchemaAndValidate(instance: JsonNode, schemaKey: SchemaKey, dataOnly: Boolean = false)(implicit resolver: Resolver): ValidatedNel[JsonNode] =
-    instance.success // TODO: implement this
+    for {
+      j  <- validateAsSelfDescribing(instance)
+      s  =  j.get("schema").asText
+      d  =  j.get("data")
+      sk <- SchemaKey.parseNel(s)
+      m  <- if (sk == schemaKey) sk.success else s"Failed to verify schema ${sk}: expected ${schemaKey}".toProcessingMessageNel.fail
+      js <- resolver.lookupSchema(m)
+      v  <- validateAgainstSchema(d, js)
+    } yield if (dataOnly) d else instance
 
   /**
    * Get our schema for self-describing Iglu instances.
@@ -224,6 +232,6 @@ class ValidatableJsonNode(instance: JsonNode) {
   def validateAndIdentifySchema(dataOnly: Boolean)(implicit resolver: Resolver): ValidatedNel[JsonSchemaPair] =
     VJM.validateAndIdentifySchema(instance, dataOnly)
 
-  def verifySchemaAndValidate(instance: JsonNode, schemaKey: SchemaKey, dataOnly: Boolean(implicit resolver: Resolver): ValidatedNel[JsonNode] =
+  def verifySchemaAndValidate(instance: JsonNode, schemaKey: SchemaKey, dataOnly: Boolean)(implicit resolver: Resolver): ValidatedNel[JsonNode] =
     VJM.verifySchemaAndValidate(instance, schemaKey, dataOnly)
 }
