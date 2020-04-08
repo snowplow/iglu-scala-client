ee0eae6ce8105b5094f48afb9a2a4620dfbd5e2e
Anton Parkhomenko <mailbox@chuwy.me>
2020-04-08 22:59:54 +0700

    Drop Scala 2.11 (WIP)

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -28,7 +28,7 @@ object BuildSettings {
   lazy val buildSettings = Seq[Setting[_]](
     organization  := "com.snowplowanalytics",
     scalaVersion  := "2.12.8",
-    crossScalaVersions  := Seq("2.11.12", "2.12.8"),
+    crossScalaVersions  := Seq("2.12.8"),
     scalacOptions := Seq(
       "-Ypartial-unification",
       "-deprecation",
