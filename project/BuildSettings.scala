a6c3ddf3d2ea4a9b783e6267b491059ccaf62792
Piotr Poniedziałek <pondzix@gmail.com>
2022-02-15 18:00:51 +0100

    Bump sbt-scoverage 1.9.3 (close #179)

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -84,7 +84,7 @@ object BuildSettings {
   )
 
   val scoverageSettings = Seq(
-    coverageMinimum := 50,
+    coverageMinimumStmtTotal := 50,
     coverageFailOnMinimum := false,
     coverageHighlighting := false,
     (Test / test) := {
