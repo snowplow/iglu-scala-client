8996359f47b26df653a7fd75141521a753c43590
Leigh-Anne Mathieson <leigh-anne@snowplowanalytics.com>
2022-08-24 12:16:13 +0100

    Allow building caches based on the resolver's content (close #207)
    I just took the code from
    
    https://github.com/snowplow/iglu-scala-client/pull/208
    
    then branched off master, and fixed the compilation errors
    
    Co-authored-by: Ian Streeter <ian@snowplowanalytics.com>
    Co-authored-by:  Piotr Poniedziałek <pondzix@gmail.com>

--- modules/core/src/main/scala/com.snowplowanalytics.iglu/client/validator/CirceValidator.scala
+++ modules/core/src/main/scala/com.snowplowanalytics.iglu/client/validator/CirceValidator.scala
@@ -14,16 +14,26 @@ package com.snowplowanalytics.iglu.client
 package validator
 
 // Scala
+import com.snowplowanalytics.iglu.client.resolver.Resolver.ResolverResult
+import com.snowplowanalytics.iglu.client.resolver.StorageTime
 import com.snowplowanalytics.iglu.core.circe.MetaSchemas
-
+// Scala
+import com.fasterxml.jackson.databind.JsonNode
+import com.snowplowanalytics.iglu.client.resolver.Resolver.SchemaLookupResult
 import scala.jdk.CollectionConverters._
 
 // Cats
+import cats.Monad
 import cats.data.{EitherNel, NonEmptyList}
 import cats.syntax.all._
 
 // Jackson
 import com.fasterxml.jackson.databind.ObjectMapper
+
+// LruMap
+import com.snowplowanalytics.lrumap.{CreateLruMap, LruMap}
+
+import com.snowplowanalytics.iglu.core.SchemaKey
 
 // JSON Schema
 import com.networknt.schema._
@@ -64,23 +74,17 @@ object CirceValidator extends Validator[Json] {
   private lazy val V4Schema =
     V4SchemaInstance.getSchema(new ObjectMapper().readTree(MetaSchemas.JsonSchemaV4Text))
 
-  def validate(data: Json, schema: Json): Either[ValidatorError, Unit] =
-    Either
-      .catchNonFatal(
-        IgluMetaschemaFactory.getSchema(circeToJackson(schema), SchemaValidatorsConfig)
-      )
-      .leftMap[ValidatorError](ValidatorError.schemaIssue) // Should never be reached in Client
+  def validate(data: Json, schema: Json): Either[ValidatorError, Unit] = {
+    val jacksonJson = circeToJackson(schema)
+    evaluateSchema(jacksonJson)
       .flatMap { schema =>
         validateOnReadySchema(schema, data).leftMap(ValidatorError.InvalidData.apply)
       }
+  }
 
   def checkSchema(schema: Json): List[ValidatorError.SchemaIssue] = {
     val jacksonJson = circeToJackson(schema)
-    V4Schema
-      .validate(jacksonJson)
-      .asScala
-      .toList
-      .map(m => ValidatorError.SchemaIssue(m.getPath, m.getMessage))
+    validateSchemaAgainstV4(jacksonJson)
   }
 
   /** Validate instance against schema and return same instance */
@@ -102,4 +106,90 @@ object CirceValidator extends Validator[Json] {
 
   private def fromValidationMessage(m: ValidationMessage): ValidatorReport =
     ValidatorReport(m.getMessage, m.getPath.some, m.getArguments.toList, m.getType.some)
+
+  private def evaluateSchema(
+    schemaAsNode: JsonNode
+  ): Either[ValidatorError.InvalidSchema, JsonSchema] = {
+    Either
+      .catchNonFatal(
+        IgluMetaschemaFactory
+          .getSchema(schemaAsNode, SchemaValidatorsConfig)
+      )
+      .leftMap(ValidatorError.schemaIssue)
+  }
+
+  private def validateSchemaAgainstV4(schema: JsonNode): List[ValidatorError.SchemaIssue] = {
+    V4Schema
+      .validate(schema)
+      .asScala
+      .toList
+      .map(m => ValidatorError.SchemaIssue(m.getPath, m.getMessage))
+  }
+
+  private[client] object WithCaching {
+
+    /**
+     * Evaluated schema in cache is identified by a schema key and a timestamp indicating when schema was cached by resolver during lookup.
+     * Compound key with timestamps allows keeping validator's schema evaluation and resolver's lookup cache in sync.
+     * See more in https://github.com/snowplow/iglu-scala-client/issues/207
+     */
+
+    type SchemaEvaluationKey         = (SchemaKey, StorageTime)
+    type SchemaEvaluationResult      = Either[ValidatorError.InvalidSchema, JsonSchema]
+    type SchemaEvaluationCache[F[_]] = LruMap[F, SchemaEvaluationKey, SchemaEvaluationResult]
+    type InitValidatorCache[F[_]]    = CreateLruMap[F, SchemaEvaluationKey, SchemaEvaluationResult]
+
+    def validate[F[_]: Monad](
+      schemaEvaluationCache: SchemaEvaluationCache[F]
+    )(data: Json, schema: SchemaLookupResult): F[Either[ValidatorError, Unit]] = {
+      getFromCacheOrEvaluate(schemaEvaluationCache)(schema)
+        .map {
+          _.flatMap { jsonschema =>
+            validateOnReadySchema(jsonschema, data)
+              .leftMap(ValidatorError.InvalidData.apply)
+          }
+        }
+    }
+
+    private def getFromCacheOrEvaluate[F[_]: Monad](
+      evaluationCache: SchemaEvaluationCache[F]
+    )(result: SchemaLookupResult): F[Either[ValidatorError.InvalidSchema, JsonSchema]] = {
+      result match {
+        case ResolverResult.Cached(key, schema, timestamp) =>
+          evaluationCache.get((key, timestamp)).flatMap {
+            case Some(alreadyEvaluatedSchema) =>
+              alreadyEvaluatedSchema.pure[F]
+            case None =>
+              provideNewJsonSchema(schema)
+                .pure[F]
+                .flatTap(result => evaluationCache.put((key, timestamp), result))
+          }
+        case ResolverResult.NotCached(schema) =>
+          provideNewJsonSchema(schema).pure[F]
+      }
+    }
+
+    private def provideNewJsonSchema(
+      schema: Json
+    ): Either[ValidatorError.InvalidSchema, JsonSchema] = {
+      val schemaAsNode = circeToJackson(schema)
+      for {
+        _         <- validateSchema(schemaAsNode)
+        evaluated <- evaluateSchema(schemaAsNode)
+      } yield evaluated
+    }
+
+    private def validateSchema(schema: JsonNode): Either[ValidatorError.InvalidSchema, Unit] = {
+      val issues = V4Schema
+        .validate(schema)
+        .asScala
+        .toList
+        .map(m => ValidatorError.SchemaIssue(m.getPath, m.getMessage))
+
+      issues match {
+        case Nil          => Right(())
+        case head :: tail => Left(ValidatorError.InvalidSchema(NonEmptyList(head, tail)))
+      }
+    }
+  }
 }
