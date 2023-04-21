c829e1b8c7cd89ec2dfcfe64a3cb6dd962a75617
Erlend Hamnaberg <erlend@hamnaberg.net>
2023-04-21 14:18:14 +0200

    use iglu: prefix to demonstrate error

--- modules/core/src/test/scala/com.snowplowanalytics.iglu.client/validator/SchemaValidationSpec.scala
+++ modules/core/src/test/scala/com.snowplowanalytics.iglu.client/validator/SchemaValidationSpec.scala
@@ -64,7 +64,7 @@ class SchemaValidationSpec extends Specification with CatsEffect {
         "jsonschema",
         SchemaVer.Full(1, 0, 0)
       ),
-      json"""{"schema": "http://iglucentral.com/schemas/com.snowplowanalytics.self-desc/schema/jsonschema/1-0-0#", "data": { "id": 0 } }"""
+      json"""{"schema": "iglu:com.snowplowanalytics.self-desc/schema/jsonschema/1-0-0", "data": { "id": 0 } }"""
     )
 
   val testResolver = SpecHelpers.TestResolver
