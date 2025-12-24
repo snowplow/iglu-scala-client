869c2700b6003ff577b221722f441bf89ae3e64d
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2025-12-24 16:42:27 +0300

    Cache found schemas forever
    Found schemas are now cached indefinitely regardless of TTL.

--- modules/core/src/test/scala/com.snowplowanalytics.iglu.client/resolver/ResolverSpec.scala
+++ modules/core/src/test/scala/com.snowplowanalytics.iglu.client/resolver/ResolverSpec.scala
@@ -278,8 +278,7 @@ class ResolverSpec extends Specification with CatsEffect {
     val responses = List(
       RegistryError.RepoFailure("Timeout exception 1").asLeft,
       RegistryError.RepoFailure("Timeout exception 2").asLeft,
-      correctSchema.asRight,
-      RegistryError.RepoFailure("Should never be reached").asLeft
+      correctSchema.asRight
     )
 
     implicit val cache: CreateResolverCache[StaticLookup] = ResolverSpecHelpers.staticResolverCache
@@ -297,7 +296,7 @@ class ResolverSpec extends Specification with CatsEffect {
       _      <- StaticLookup.addTime(2.seconds)
       _      <- resolver.lookupSchema(schemaKey)
       _      <- StaticLookup.addTime(2.seconds)
-      result <- resolver.lookupSchema(schemaKey) // ... but don't try to overwrite it
+      result <- resolver.lookupSchema(schemaKey)
     } yield result
 
     val (state, response) = result.run(ResolverSpecHelpers.RegistryState.init).value
@@ -305,8 +304,9 @@ class ResolverSpec extends Specification with CatsEffect {
     // Final response must not overwrite a successful one
     val finalResult = response must beRight(correctSchema)
 
-    // Check that it attempted to get fourth schema (500 response)
-    val lookupTries = state.req must beEqualTo(4)
+    // Found schemas are cached forever - only 3 requests needed (2 failures + 1 success)
+    // No 4th request because successful result is cached forever
+    val lookupTries = state.req must beEqualTo(3)
 
     finalResult and lookupTries
   }
