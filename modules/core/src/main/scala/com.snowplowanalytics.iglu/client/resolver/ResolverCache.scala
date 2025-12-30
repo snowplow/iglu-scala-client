1eeb46738f7819a3fdf635094affc2f512c7dffd
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2025-12-30 12:52:12 +0300

    Revert "Cache found schemas forever (#272)" (#275)
    This reverts commit 6c9d9fc59b5f403a1f3f007d6be2586342ed462c.

--- modules/core/src/main/scala/com.snowplowanalytics.iglu/client/resolver/ResolverCache.scala
+++ modules/core/src/main/scala/com.snowplowanalytics.iglu/client/resolver/ResolverCache.scala
@@ -212,7 +212,7 @@ object ResolverCache {
             Some(TimestampedItem(lookup, storageTime))
           else {
             lookup match {
-              case Right(_) => Some(TimestampedItem(lookup, storageTime))
+              case Right(_) => None
               case Left(failures) =>
                 val noNotFounds = failures.map { case (k, history) =>
                   k -> history.copy(errors = history.errors - RegistryError.NotFound)
